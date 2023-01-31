7df962e5296e56fa40c71a5b67e744274fdfb5eb
a-menshikov <a.menshikov1989@gmail.com>
2023-01-31 20:18:05 +0300

    стартанули аппы

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig'
 ]
 
 MIDDLEWARE = [
