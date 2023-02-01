e42086e6548cf2c783a9ba1bf17a4028e5b4653f
a-menshikov <a.menshikov1989@gmail.com>
2023-02-01 10:39:43 +0300

    добавлена структура view

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+# from reviews.models import Category, Genre, Title
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Category."""
+
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Genre."""
+
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Title."""
+
+    serializer_class = TitleSerializer
