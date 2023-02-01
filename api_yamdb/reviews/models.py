bea0c69b1e27f46889ef9bd6526f676ba5734123
a-menshikov <a.menshikov1989@gmail.com>
2023-02-01 10:30:29 +0300

    добавлена структура моделей

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель категории."""
+    pass
+
+    def __str__(self):
+        """Возвращает _______."""
+        pass
+
+
+class Genre(models.Model):
+    """Модель жанра."""
+    pass
+
+    def __str__(self):
+        """Возвращает _______."""
+        pass
+
+
+class Title(models.Model):
+    """Модель тайтла."""
+    pass
+
+    def __str__(self):
+        """Возвращает _______."""
+        pass
