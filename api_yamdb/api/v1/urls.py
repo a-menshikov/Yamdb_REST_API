0e000543d5aeba865cd42077f69d72adcbc9226b
a-menshikov <a.menshikov1989@gmail.com>
2023-02-01 10:43:32 +0300

    новые view добавлены в роутер

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-# from .views import CommentsViewSet, FollowViewSet, GroupViewSet, PostViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 router = routers.DefaultRouter()
+
+router.register('categories', CategoryViewSet, basename='category')
+router.register('genres', GenreViewSet, basename='genre')
+router.register('titles', TitleViewSet, basename='title')
 
 urlpatterns = [
     path('', include(router.urls)),
