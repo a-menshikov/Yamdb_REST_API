049edbea372fb56d429afecd091c8394199d0591
a-menshikov <a.menshikov1989@gmail.com>
2023-02-02 11:01:58 +0300

    добавлены сериализаторы - требуют проверки

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,26 +1,53 @@
 from rest_framework import serializers
 from reviews.models import Category, Genre, Title
+import re
+from django.utils import timezone
 
 
 class CategorySerializer(serializers.ModelSerializer):
     """Сериализатор модели Category."""
 
+    def validate_slug(self, value):
+        """Проверка соответствия слага категории."""
+        if not re.fullmatch(r'^[-a-zA-Z0-9_]+$', value):
+            raise serializers.ValidationError(
+                'Псевдоним категории не соотвествует формату'
+            )
+        return value
+
     class Meta:
-        # fields = ()
+        fields = ('name', 'slug')
         model = Category
 
 
 class GenreSerializer(serializers.ModelSerializer):
     """Сериализатор модели Genre."""
 
+    def validate_slug(self, value):
+        """Проверка соответствия слага жанра."""
+        if not re.fullmatch(r'^[-a-zA-Z0-9_]+$', value):
+            raise serializers.ValidationError(
+                'Псевдоним жанра не соотвествует формату'
+            )
+        return value
+
     class Meta:
-        # fields = ()
+        fields = ('name', 'slug')
         model = Genre
 
 
 class TitleSerializer(serializers.ModelSerializer):
     """Сериализатор модели Title."""
 
+    def validate_year(self, value):
+        """Проверка года на будущее время."""
+        current_year = timezone.now().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                'Марти, ты опять взял Делориан без спроса?!'
+            )
+        return value
+
     class Meta:
-        # fields = ()
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
         model = Title
