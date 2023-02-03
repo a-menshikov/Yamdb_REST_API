fd448dda55059b6fa96b7c804977d86c3d6d16ed
a-menshikov <a.menshikov1989@gmail.com>
2023-02-03 10:29:11 +0300

    настройки для пагинации, изменен дефолтный пермишн

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -109,11 +109,12 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'PAGE_SIZE': 10,
 }
 
 SIMPLE_JWT = {
