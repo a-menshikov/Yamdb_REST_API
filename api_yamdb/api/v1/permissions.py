89741a66487b734db435578f36f554418a1682bb
a-menshikov <a.menshikov1989@gmail.com>
2023-02-03 14:27:14 +0300

    пермишн для моделей ветки base

---
+++ api_yamdb/api/v1/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """Даёт доступ неадмину только к GET/OPTIONS/HEAD."""
+
+    message = 'Данный запрос недоступен для вас.'
+
+    def has_permissions(self, request, view):
+        """Проверка на запросы к объекту
+        Для безопасных методов всегда True."""
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and (
+                    request.user.role == 'ADM'
+                    or request.user.is_staff
+                    or request.user.is_superuser
+                )
+            )
+        )
