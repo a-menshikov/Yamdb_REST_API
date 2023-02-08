4ec480dcdd5a99aa6e544919ec34af6724e3f79c
Артём Меньшиков <112660785+a-menshikov@users.noreply.github.com>
2023-02-08 20:20:04 +0300

    Update admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -28,13 +28,8 @@ class GenreAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
-class GenreTitleInline(admin.TabularInline):
-    model = GenreTitle
-
-
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    inlines = (GenreTitleInline,)
     list_display = (
         'pk',
         'name',
