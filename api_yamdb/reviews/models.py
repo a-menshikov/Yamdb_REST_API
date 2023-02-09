cc7a3943df0a2a581fbfc020e45707d8882ad920
a-menshikov <a.menshikov1989@gmail.com>
2023-02-09 12:06:27 +0300

    isort

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,4 @@
-from django.core.validators import (
-    MinValueValidator,
-    MaxValueValidator,
-)
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from reviews.validators import validate_year
